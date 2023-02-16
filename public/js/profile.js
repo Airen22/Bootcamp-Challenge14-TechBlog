c0a3b6e588d0fe940da451c68e7f9c54e217f446
Erin <erinpeifer@gmail.com>
2023-02-15 22:13:31 -0600

    debug new post - updated to catch post_content

--- public/js/profile.js
+++ public/js/profile.js
@@ -2,12 +2,12 @@ const newFormHandler = async (event) => {
     event.preventDefault();
   
     const title = document.querySelector('#post-name').value.trim();
-    const content = document.querySelector('#post-desc').value.trim();
+    const post_content = document.querySelector('#post-content').value.trim();
   
-    if (title && content) {
+    if (title && post_content) {
       const response = await fetch('/api/posts', {
         method: 'POST',
-        body: JSON.stringify({ title, content }),
+        body: JSON.stringify({ title, post_content }),
         headers: {
           'Content-Type': 'application/json',
         },

512979835afede9176170018f88acfd1e8ccdef9
Erin <erinpeifer@gmail.com>
2023-02-15 19:10:01 -0600

    seeded database

--- models/Comment.js
+++ models/Comment.js
@@ -11,7 +11,7 @@ Comment.init(
         primaryKey: true,
         autoIncrement: true,
       },
-      commment_content: {
+      comment_content: {
         type: DataTypes.STRING,
       },
       date_created: {
--- seeds/seed.js
+++ seeds/seed.js
@@ -1,8 +1,9 @@
 const sequelize = require('../config/connection');
-const { User, Post } = require('../models');
+const { User, Post, Comment} = require('../models');
 
 const userData = require('./userData.json');
 const postData = require('./postData.json');
+const commentData = require('./commentData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -12,13 +13,13 @@ const seedDatabase = async () => {
     returning: true,
   });
 
-  for (const post of postData) {
-    await Post.create({
-      ...post,
-    });
-  }
+  const posts = await Post.bulkCreate(postData);
+
+  
+  const comments = await Comment.bulkCreate(commentData)
 
   process.exit(0);
 };
+
 
 seedDatabase();
